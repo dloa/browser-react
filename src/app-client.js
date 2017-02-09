3e49e900281e497cdd152efbbeece84203dd498c
OstlerDev <ostlerdev@me.com>
2017-02-09 12:34:39 -0700

    Push up base outline
    Does not include SSR or any regular browser code.

---
+++ src/app-client.js
@@ -0,0 +1,9 @@
+// https://scotch.io/tutorials/react-on-the-server-for-beginners-build-a-universal-react-and-node-app
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRoutes from './components/AppRoutes';
+
+window.onload = () => {
+  ReactDOM.render(<AppRoutes/>, document.getElementById('main'));
+};
