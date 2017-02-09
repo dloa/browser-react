e8d5a717e0bd5edcfdf6137245c2d4cf542ca5c8
OstlerDev <ostlerdev@me.com>
2017-02-09 16:48:04 -0700

    Change formatting

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -4,15 +4,15 @@ import React from 'react';
 //import athletes from '../data/athletes';
 
 export default class IndexPage extends React.Component {
-  render() {
-    return (
-      <div className="home">
-        <div className="athletes-selector">
-          
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="home">
+				<div className="athletes-selector">
+					
+				</div>
+			</div>
+		);
+	}
 }
 
 //{athletes.map(athleteData => <AthletePreview key={athleteData.id} {...athleteData} />)}
