fcc669d7a965b91dd26791f6dde5e7491293941b
OstlerDev <ostlerdev@me.com>
2017-02-09 17:01:15 -0700

    Fix HTML formatting

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import NotFoundPage from './components/NotFoundPage';
 const routes = (
 	<Route path="/" component={Layout}>
 		<IndexRoute component={BrowsePage}/>
- 	
+ 		<Route path="#/:hash" component={BrowsePage}/>
 		<Route path="*" component={NotFoundPage}/>
 	</Route>
 );
