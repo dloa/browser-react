7eae7f7fdb6ea5ad3febe815a08d40962d3053d0
OstlerDev <ostlerdev@me.com>
2017-03-29 13:58:56 -0600

    Fix imports

--- src/static/js/wallet-flovault.js
+++ src/static/js/wallet-flovault.js
@@ -1,11 +1,11 @@
 // FLOVAULT INIT
 function FloVaultInit() {
-	loadScript(document.location.origin + '/js/SimpleWallet.js', SimpleWallet_loaded);
+	loadScript(document.location.origin + '/static/js/SimpleWallet.js', SimpleWallet_loaded);
 }
 
 function SimpleWallet_loaded() {
 	console.log('SimpleWallet.js loaded');
-	loadScript(document.location.origin + '/js/SimpleDeps.js', SimpleDeps_loaded);
+	loadScript(document.location.origin + '/static/js/SimpleDeps.js', SimpleDeps_loaded);
 };
 
 function SimpleDeps_loaded() {
