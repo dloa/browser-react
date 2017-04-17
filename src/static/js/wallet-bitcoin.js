fb4b0c9957e32177656806aa2e123bccc01b3439
OstlerDev <ostlerdev@me.com>
2017-04-17 14:57:37 -0600

    remove a console.log

--- src/static/js/wallet-bitcoin.js
+++ src/static/js/wallet-bitcoin.js
@@ -3,7 +3,7 @@ var btc_wallet;
 function loadWallet(){
 	// Load wallet stored in Local Storage
 	var BTC_Wal = localStorage.getItem("btc_wallet");
-	console.log(BTC_Wal);
+	//console.log(BTC_Wal);
 	if (BTC_Wal != null && BTC_Wal != undefined && BTC_Wal != ""){
 		console.log("Login");
 		// Login
